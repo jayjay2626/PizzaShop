63e2c9a92ca6438a1ffa5e052407f397c7fb59e2
Rangeley Jay Delphin <Rangeleydelphin@gmail.com>
2019-06-25 09:24:12 -0400

    Initial Solution Setup

---
+++ PizzaShop.WebUI/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PizzaShop.WebUI
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
