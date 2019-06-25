1fc0d4e9185824f3fd4f8fa1a8cfc51eed6fe813
Rangeley Jay Delphin <Rangeleydelphin@gmail.com>
2019-06-25 11:42:00 -0400

    Pizza Model and InMemory Pizza Repository Built

---
+++ PizzaShop.Core/Models/Pizza.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaShop.Core.Models
+{
+    public class Pizza
+    {
+        // Id is of string type because we are going to
+        // auto-generate an ID based off GUID instead
+        // of letting the database generate the ID
+        public string Id { get; set; }
+
+        [StringLength(30)]
+        [DisplayName("Product Name")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [Range(0, 1000)]
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+
+        public Pizza()
+        {
+            // Auto-generate the 
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ PizzaShop.DataAccess.InMemory/PizzaRepository.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+using PizzaShop.Core.Models;
+
+namespace PizzaShop.DataAccess.InMemory
+{
+    public class PizzaRepository
+    {
+        // First thing to do is create an ObjectCache
+        ObjectCache cache = MemoryCache.Default;
+
+        // Internal List of Pizza
+        List<Pizza> pizzas;
+
+        public PizzaRepository()
+        {
+            pizzas = cache["pizzas"] as List<Pizza>;
+            if (pizzas == null)
+            {
+                pizzas = new List<Pizza>();
+            }
+        }
+
+        public void Commit()
+        {
+            cache["pizzas"] = pizzas;
+        }
+
+        public void Insert(Pizza p)
+        {
+            pizzas.Add(p);
+        }
+
+        public void Update(Pizza pizza)
+        {
+            Pizza pizzaToUpdate = pizzas.Find(p => p.Id == pizza.Id);
+
+            if (pizzaToUpdate != null)
+            {
+                pizzaToUpdate = pizza;
+            } else
+            {
+                throw new Exception("Pizza not found.");
+            }
+        }
+
+        public Pizza Find(string Id)
+        {
+            Pizza pizza = pizzas.Find(p => p.Id == Id);
+
+            if (pizza != null)
+            {
+                return pizza;
+            }
+            else
+            {
+                throw new Exception("Pizza not found.");
+            }
+        }
+
+        public IQueryable<Pizza> Collection()
+        {
+            return pizzas.AsQueryable();
+        }
+
+        public void Delete(string Id)
+        {
+            Pizza pizzaToDelete = pizzas.Find(p => p.Id == Id);
+
+            if (pizzaToDelete != null)
+            {
+                pizzas.Remove(pizzaToDelete);
+            }
+            else
+            {
+                throw new Exception("Pizza not found.");
+            }
+        }
+    }
+}
